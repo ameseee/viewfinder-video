3808882fd0523d09e321bdf225a8959011668f2e
Amy Holt <amy.elizabeth.holt@gmail.com>
2019-05-22 16:37:28 -0600

    Initial commit

---
+++ ViewFinder-Video/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ViewFinder-Video
+//
+//  Created by Amy Holt on 5/22/19.
+//  Copyright © 2019 Amy Holt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
